e7ba5de8d28cf5e8cc5b33bdd709e4ae947e856f
zjthomas <zachthomas809@gmail.com>
2019-07-17 21:55:30 -0700

    add ec2 route for photos

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const app = express();
 const port =  3000;
 
 app.use(express.static(path.join(__dirname, '../public')))
-app.use('/photos', proxy('http://localhost:3001'));
+app.use('/photos', proxy('http://ec2-18-222-224-216.us-east-2.compute.amazonaws.com/photos/'));
 // app.use('/', proxy('http://localhost:3002'));
 app.use('/reviews', proxy('http://localhost:3003'));
 // app.use('/', proxy('http://localhost:3004'));
